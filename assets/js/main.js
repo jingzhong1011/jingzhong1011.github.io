56879aba5fb695fe9e6978dc7a8114c5051c418d
Jing-Zhong Wang <75709896+jingzhong1011@users.noreply.github.com>
2025-12-23 03:37:18 +0800

    progress bar added

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,16 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
+
+window.onscroll = function() { updateProgressBar() };
+
+function updateProgressBar() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  
+  const progressBar = document.getElementById("myBar");
+  if (progressBar) {
+    progressBar.style.width = scrolled + "%";
+  }
+}
